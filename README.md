32ac75214dcb33bfd33c27617335badd77061850
Kevin Tran <kevintraan@live.com>
2020-11-16 20:39:35 -0700

    Added project and updated README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# PropertyAssessment
+# Property Assessment App
+
+>## This application takes in an Edmonton address and returns the property assessment based on that location.  
+
+
+---
+
+
+## Technology Stack:
+![Java](/icons/java.png) ![Vue](/icons/vue.png) ![SQL](/icons/sql.png)
+
---
+++ dmit2015-winter2020term-sectiona01-assignment04-ktraan-master/dmit2015-assignment04-ktran36/src/test/java/dmit2015/ktran36/assignment04/SampleTest.java
@@ -0,0 +1,14 @@
+package dmit2015.ktran36.assignment04;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleTest {
+
+	@Test
+	public void simpleJUnit5Test() {
+		String result = "duke";
+		assertEquals("duke", result);
+	}
+}
