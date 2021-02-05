bd1ad3b445bf010c24d6434f45273ac40408fa17
Kevin Tran <kevintraan@live.com>
2021-02-05 13:39:41 -0700

    Fetching assessment API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Row from 'react-bootstrap/Row';
 import Form from './components/Form';
 
 function App() {
-  const [address, setAddress] = useState('');
+  const [assessment, setAssessment] = useState('');
+  const [address, setAddress] = useState({
+    houseNumber: '',
+    streetName: '',
+  });
+
   const BASE_URL_ENDPOINT = 'https://data.edmonton.ca/resource/q7d6-ambg.json?';
 
+  // Get assessment from props
   const getAssessment = async (props) => {
-    const fullAddress = `${props.houseNumber} ${props.streetName}`;
-    setAddress(fullAddress);
-    console.log(address);
+    const houseNumber = props.houseNumber;
+    const streetName = props.streetName.toUpperCase();
 
-    // setAddress(`${props.houseNumber} ${props.streetName}`);
-    // console.log(`Full address: ${address}`);
+    // Set the full address to state
+    setAddress({ houseNumber: houseNumber, streetName: streetName });
+
+    fetchAssessment(houseNumber, streetName);
+  };
+
+  // Fetch assessment
+  const fetchAssessment = async (houseNumber, streetName) => {
+    const res = await fetch(
+      `${BASE_URL_ENDPOINT}house_number=${houseNumber}&street_name=${streetName}`
+    );
+    console.log(res);
+    const data = await res.json();
+    console.log(data);
   };
 
   return (
